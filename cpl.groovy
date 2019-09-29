53937090c43d83ea9889ea2f362c272d30282efe
jayabalandevops <balaniaf@gmail.com>
2019-09-29 21:00:38 +0530

    cpl groovy script added

---
+++ cpl.groovy
@@ -0,0 +1,28 @@
+folder ("$BUName") {}
+folder ("$BUName/$ProductName") {}
+pipelineJob("$BUName/$ProductName/CICD_$AppName"){
+
+    parameters{
+        stringParam("AppRepo","$ApplicationRepo","Git URL")
+        stringParam("UnitTestTool", "$UnitTestRun", "")
+        stringParam("NodeName", "$NodeName", "")
+        stringParam("AppName", "$AppName", "")
+        activeChoiceParam('Branch') {
+            description('Select the branch')
+            filterable()
+            choiceType('SINGLE_SELECT')
+            groovyScript {
+                script('["master", "develop"]')
+                fallbackScript('"fallback choice"')
+            }
+        }
+    }
+    definition {
+        cps {
+            def jobScript = readFileFromWorkspace('cit.groovy')
+            script(jobScript)
+            def approvals = org.jenkinsci.plugins.scriptsecurity.scripts.ScriptApproval.get()
+			approvals.approveScript(approvals.hash(jobScript,"groovy")
+        }
+    }
+}

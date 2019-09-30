c27cb691046181c80f425ef670af273b4472f021
jayabalandevops <balaniaf@gmail.com>
2019-09-30 15:10:52 +0530

    shell command changes 4

--- app/build.groovy
+++ app/build.groovy
@@ -7,5 +7,5 @@ def bStr = jString.btool
 bStr = "$bStr" + " -f " + '"appl/pom.xml"' + " " + jString.target
 println("Executing the mvn clean package command...")
 println("$bStr")
-shell "$bStr"
+sh "$bStr"
 println("Completed the Building of the Application!")

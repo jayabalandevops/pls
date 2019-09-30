c08989aa027d7147d4200be1ef5ab437f45f01ee
jayabalandevops <balaniaf@gmail.com>
2019-09-30 06:20:43 +0530

    cit

---
+++ cit.groovy
@@ -0,0 +1,17 @@
+node ("$NodeName") {
+    wrks = env.WORKSPACE
+    stage ("Prepare") {
+    println("Preparing...")
+    git(
+        url: git@github.com:jayabalandevops/pls.git,
+        branch: "master"
+    )
+    dir ('config') {
+    git(
+        url: git@github.com:jayabalandevops/pls.git,
+        branch: master
+    )
+    }
+    println("Prepared..!")
+    }
+}

b55c7ef3d636fae814e0fe094d76bd353dd01ce1
jayabalandevops <balaniaf@gmail.com>
2019-09-30 10:42:34 +0530

    build.groovy bstr

--- app/build.groovy
+++ app/build.groovy
@@ -4,8 +4,8 @@ println("Building the Application...")
 cDelivery = readFile "$wrks/config/cpl.json"
 def jString = new JsonSlurperClassic().parseText(cDelivery)
 def bStr = jString.btool
-bStr = bStr + " -f appl/pom.xml " + jString.target
+bStr = "$bStr" + " -f appl/pom.xml " + jString.target
 println("Executing the mvn clean package command...")
 println("$bStr")
-shell "$bStr"
+shell ("$bStr")
 println("Completed the Building of the Application!")

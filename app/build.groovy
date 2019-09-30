af0a221f98a34f4d1c64b2a4a7633932f1885de5
jayabalandevops <balaniaf@gmail.com>
2019-09-30 09:59:00 +0530

    added println statement to build script

--- app/build.groovy
+++ app/build.groovy
@@ -1,7 +1,10 @@
 import groovy.json.JsonSlurperClassic
 
+println("Building the Application...")
 cDelivery = readFile "$wrks/config/cpl.json"
 def jString = new JsonSlurperClassic().parseText(cDelivery)
 def bStr = jString.btool
 bStr = bStr + " -f appl/pom.xml " + jString.target
+println("Executing the mvn clean package command...")
 shell "$bStr"
+println("Completed the Building of the Application!")

ca758721cddaad9662dd533ba698fda2df9d9639
jayabalandevops <balaniaf@gmail.com>
2019-10-02 12:34:27 +0530

    added dep.sh the copy destination

--- scripts/dep.sh
+++ scripts/dep.sh
@@ -1,2 +1,2 @@
 rm -rf /opt/servers/tomcat8/webapps/arapp
-cp -r /home/jayabalan/.jenkins/workspace/Insurance/UnitLinked/CICD_jjn/appl/target/arapp.war
+cp -r /home/jayabalan/.jenkins/workspace/Insurance/UnitLinked/CICD_jjn/appl/target/arapp.war /opt/servers/tomcat8/webapps/
